33db01bf92ddba31b9ee42fc51300c63c0d3eed4
takanew <145203231+takanew@users.noreply.github.com>
2023-11-12 07:34:27 +0000

    [Finish]2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   
   config.hosts << "39628ba9e0e74a95ab148263ac1ad20d.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
+  
 end
