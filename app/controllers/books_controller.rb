45b8cdc1e70ea3055835253840d95853a19a0f8f
takanew <145203231+takanew@users.noreply.github.com>
2023-11-14 05:57:57 +0000

    [Finish]9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/show'
+  get 'books/index'
+  get 'books/create'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destroy'
   root to: "homes#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
