a4e2661e9c2ddf6508fc18cb59a2deda94f02a29
takanew <145203231+takanew@users.noreply.github.com>
2023-11-16 03:46:13 +0000

    [Finish]13

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-  get 'users/show'
-  get 'users/index'
-  
-  get 'books/show'
-  get 'books/index'
-  get 'books/create'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
-  
   root to: "homes#top"
   devise_for :users
+  resources :books, only: [:show, :index, :create, :edit, :update, :destroy]
+  resources :users, only: [:edit, :show, :index]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
