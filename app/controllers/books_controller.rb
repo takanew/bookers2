5277d86ae73b389305917d33b40f6d718ab224b7
takanew <145203231+takanew@users.noreply.github.com>
2023-11-18 07:52:39 +0000

    暫定終了21

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,21 @@ class BooksController < ApplicationController
   end
 
   def index
+    # 新規投稿フォーム用インスタンス
+    @book = Book.new
+    
+    @user = current_user
   end
 
   def create
+    # データを受け取り新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # use_idカラムにcurrent_user.idを代入
+    @book.user_id = current_user.id
+    # データをデータベースに保存するためのsaveメソッド実行
+    @book.save
+    # 詳細画面へリダイレクト
+    redirect_to book_path(@book)
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
 
-  resources :users
+  resources :users, only: [:edit, :show, :index]
   resources :books, only: [:show, :index, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get 'home/about' => "homes#about" , as: 'about'
 end
